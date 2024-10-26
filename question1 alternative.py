0c021c1c86a58ad7ff90dbeee03fea90a4c338ab
Nasho99 <kimeunashon@gmail.com>
2024-10-26 12:06:07 +0300

    added alternative solution for question1

---
+++ question1 alternative.py
@@ -0,0 +1,13 @@
+def fizz_buzz():
+    for num in range(1, 51):  # Loop through numbers 1 to 50
+        if num % 3 == 0 and num % 5 == 0:  # Check if divisible by both 3 and 5
+            print("FizzBuzz")
+        elif num % 3 == 0:  # Check if divisible by 3
+            print("Fizz")
+        elif num % 5 == 0:  # Check if divisible by 5
+            print("Buzz")
+        else:
+            print(num)  # Print the number if not divisible by 3 or 5
+
+# Call the function to execute
+fizz_buzz()
--- question1.py
+++ question1.py
@@ -1,3 +1,4 @@
+
 for num in range(1, 51):  # Loop through numbers 1 to 50
     if num % 3 == 0 and num % 5 == 0:  # Check if divisible by both 3 and 5
         print("FizzBuzz")
